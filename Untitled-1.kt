3b09aef11157bf327af4b32213ae78332eba7c9c
Jonathan <jonulisanpar@gmail.com>
2021-06-23 09:50:04 -0500

    first commit

---
+++ Untitled-1.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val numero1 = 3
+    val numero2 = 5
+    
+    println("Suma = " + sumar(numero1, numero2))
+}
+
+
+fun sumar(num1: Int, num2: Int) : Int{
+    return num1 + num2
+}
---
+++ prueba1.kts
@@ -0,0 +1,10 @@
+fun main() {
+    val numero1 = 6
+    val numero2 = 9
+
+    println("La repuesta es: " + sumar(numero1, numero2))
+}
+
+fun sumar(num1 : Int, num2 : Int) : Int{
+    return num1 + num2
+}
